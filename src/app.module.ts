5c8232a4f0240a830f4f252d0a924c8bc6d76e19
anuj-jft <anuj.singh@jellyfishtechnologies.com>
2024-10-01 17:43:51 +0530

    db configuration done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '@Shivam123',
+    database: 'server',
+    entities: [],
+    synchronize: true,
+  }),],
   controllers: [],
   providers: [],
 })
